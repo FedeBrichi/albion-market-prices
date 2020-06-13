e755125419ac4d9c31d3f6097c7d73b0f9afcdcd
Federico Zanellato <zanellato.federico@gmail.com>
2020-06-13 18:42:00 +0200

    enhance card style and favicon

--- src/components/ItemCard.tsx
+++ src/components/ItemCard.tsx
@@ -1,43 +1,69 @@
 import React from 'react';
-import { Paper, makeStyles, Typography, Grid } from '@material-ui/core';
-// import { Favorite, FavoriteBorder } from '@material-ui/icons';
+import {
+  makeStyles,
+  Typography,
+  IconButton,
+  Card,
+  CardContent,
+  CardMedia,
+  createStyles,
+  CardActionArea,
+} from '@material-ui/core';
+import { FavoriteBorder } from '@material-ui/icons';
+import { Link } from 'react-router-dom';
 
 type Props = {
   id: string;
   name: string;
 };
 
-const useStyles = makeStyles(theme => ({
-  paper: {
-    padding: theme.spacing(2),
-    color: theme.palette.secondary.dark,
-    backgroundColor: theme.palette.secondary.light,
-    margin: '0.5rem 0.5rem',
-  },
-  icon: {
-    position: 'relative',
-    top: 0,
-    left: 0,
-    height: '70px',
-    width: '70px',
-  },
-  name: {},
-}));
+const useStyles = makeStyles(theme =>
+  createStyles({
+    root: {
+      color: theme.palette.secondary.dark,
+      backgroundColor: theme.palette.secondary.light,
+      margin: '0.5rem 0.5rem',
+    },
+    details: {
+      display: 'flex',
+      flexDirection: 'column',
+    },
+
+    icon: {
+      float: 'left',
+      height: '70px',
+      width: '70px',
+    },
+    playIcon: {
+      margin: 0,
+      padding: 0,
+      height: 25,
+      width: 25,
+    },
+  }),
+);
 
 export const ItemCard = ({ id, name }: Props) => {
   const classes = useStyles();
   const imgUrl = `http://gameinfo.albiononline.com/api/gameinfo/items/${id}`;
 
   return (
-    <Paper elevation={3} className={classes.paper}>
-      <Grid container spacing={1}>
-        <Grid item>
-          <img src={imgUrl} className={classes.icon} alt={name} />
-        </Grid>
-        <Grid item>
-          <Typography className={classes.name}>{name}</Typography>
-        </Grid>
-      </Grid>
-    </Paper>
+    <Card className={classes.root}>
+      <Link to={`/details/${id}`} style={{ textDecoration: 'none' }}>
+        <CardActionArea>
+          <CardMedia className={classes.icon} image={imgUrl} title={name} />
+          <div className={classes.details}>
+            <CardContent>
+              <Typography align="left" color="primary">
+                {name}
+              </Typography>
+            </CardContent>
+          </div>
+        </CardActionArea>
+      </Link>
+      <IconButton style={{ float: 'right' }}>
+        <FavoriteBorder className={classes.playIcon} />
+      </IconButton>
+    </Card>
   );
 };
--- src/components/ItemList.tsx
+++ src/components/ItemList.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Item } from '../types/Item';
-import { Link } from 'react-router-dom';
 import { ItemCard } from './ItemCard';
 import { FixedSizeGrid, GridChildComponentProps } from 'react-window';
 
@@ -12,11 +11,7 @@ type Props = {
 
 export const ItemList = ({ items }: Props) => {
   const itemGrid = items.map(item => {
-    return (
-      <Link to={`/details/${item.id}`} style={{ textDecoration: 'none' }}>
-        <ItemCard {...item} />
-      </Link>
-    );
+    return <ItemCard {...item} />;
   });
 
   const Row = ({ columnIndex, rowIndex, style }: GridChildComponentProps) => {
@@ -25,7 +20,7 @@ export const ItemList = ({ items }: Props) => {
 
   const getRowHeight = (width: number) => {
     if (width >= 1000) return 130;
-    if (width >= 700) return 150;
+    if (width >= 770) return 150;
     if (width >= 500) return 180;
     return 200;
   };
